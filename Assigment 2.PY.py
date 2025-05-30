8a33cdb3a81f71845fb3a0554ab0815ad988e5eb
Mister-Jordan <misterprathmesh@gmail.com>
2025-05-31 04:55:15 +0530

    Add files via upload
    ASSIGNMENT 2:
    Module 3: Control Structures in Python

---
+++ Assigment 2.PY.py
@@ -0,0 +1,25 @@
+#Task 1: Check if a Number is Even or Odd
+
+
+number = int(input("ENTER THE NUMBER: "))
+if number % 2==0:
+    print("The Number is EVEN")
+else:
+    print("THE NUMBER IS ODD")
+
+#Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+
+
+The_total_sum=0
+
+for Sum in range(1,51):
+     The_total_sum += Sum
+print("The sum of total numbers from 1 to 50: ",The_total_sum)
+
+print("Thank you for this assigment")
+print("Type 'okay' below")
+input("Enter the word: ")
+print("IT was ACTUALLY little TOUGH")
+print("I took the help of CHAT GPT to understand some formulas for getting odd and even number")
+print('Hope it is not a cheating')
